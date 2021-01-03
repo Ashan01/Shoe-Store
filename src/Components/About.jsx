0113a43106ce65a14bcf6278690e2b6bc4b031e7
Ashan Jameel <59074253+Ashan01@users.noreply.github.com>
2021-01-04 04:17:32 +0500

    About and Home change

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React from "react";
 
 export const About = () => {
-    return (
-        <div>
-            <h3>Im About</h3>
-        </div>
-    )
-}
+   return (
+      <div>
+         <h3> About</h3>
+      </div>
+   );
+};
